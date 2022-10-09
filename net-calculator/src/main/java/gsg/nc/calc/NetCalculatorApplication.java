784f12800a52605d3d7f853abfdeae6f351ef387
Rishi Raj <rishiraj88+github@gmail.com>
2022-10-09 12:17:51 +0200

    Initial setup of Net Calculator service.

---
+++ net-calculator/src/main/java/gsg/nc/calc/NetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package gsg.nc.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetCalculatorApplication.class, args);
+	}
+
+}
---
+++ net-calculator/src/test/java/gsg/nc/calc/NetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package gsg.nc.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
